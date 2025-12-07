ed4d8dff86b77d801892fff48b801114e43965a0
cybersecbella <rizzy05.m@gmail.com>
2025-12-07 18:54:50 -0500

    ratelimiter_insecurebackend

--- fastbank_insecure_classwork/backend/server.js
+++ fastbank_insecure_classwork/backend/server.js
@@ -85,6 +85,18 @@ function auth(req, res, next) {
 // Q4 — AUTH ISSUE 3: Username enumeration.
 // Q4 — AUTH ISSUE 4: Predictable sessionId.
 // ------------------------------------------------------------
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
