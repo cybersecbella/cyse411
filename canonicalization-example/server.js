781e29cf12dfa9c9456b85877d7d72576489a15e
cybersecbella <rizzy05.m@gmail.com>
2025-12-08 07:46:40 -0500

    define Root

--- canonicalization-example/server.js
+++ canonicalization-example/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { body, validationResult } = require('express-validator');
+const ROOT = "/var/www/";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
